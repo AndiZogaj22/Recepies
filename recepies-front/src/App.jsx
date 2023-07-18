2b07e6e7b887e07a939353341ca02128fc0c6795
AndiZogaj22 <andiolsizogaj@gmail.com>
2023-07-18 22:58:05 +0200

    recepies app files

---
+++ recepies-front/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import SearchBar from './Components/SearchBar'
+import Categories from './Components/Categories'
+
+function App() {
+
+  return (
+    <>
+      <SearchBar />
+      
+      <Categories />
+    </>
+  )
+}
+
+export default App
