d48aca8a8d9f085ca23f849cf6d3363da0a74781
AndiZogaj22 <andiolsizogaj@gmail.com>
2023-07-23 22:05:19 +0200

    mongo connection fixed

--- recepies-api/app.js
+++ recepies-api/app.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/recipesDB', {
+mongoose.connect('mongodb+srv://AndiZogaj:Ub9jMkhLWMO92A8r@andizogaj.tmvniin.mongodb.net/', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -29,29 +29,29 @@ const Recipe = mongoose.model('Recipe', recipeSchema);
 app.use(bodyParser.json());
 
 // Route to get all recipes
-app.get('/api/recipes', (req, res) => {
-  Recipe.find({}, (err, recipes) => {
-    if (err) {
-      return res.status(500).json({ error: 'Internal server error.' });
-    }
+app.get('/api/recipes', async (req, res) => {
+  try {
+    const recipes = await Recipe.find({});
     res.json(recipes);
-  });
+  } catch (err) {
+    res.status(500).json({ error: 'Internal server error.' });
+  }
 });
 
 // Route to add a new recipe
-app.post('/api/recipes', (req, res) => {
+app.post('/api/recipes', async (req, res) => {
   const { name, description, ingredients, country, image } = req.body;
   if (!name || !description || !ingredients || !country || !image) {
     return res.status(400).json({ error: 'Please provide all required fields.' });
   }
 
-  const newRecipe = new Recipe({ name, description, ingredients, country, image });
-  newRecipe.save((err, savedRecipe) => {
-    if (err) {
-      return res.status(500).json({ error: 'Failed to save the recipe.' });
-    }
+  try {
+    const newRecipe = new Recipe({ name, description, ingredients, country, image });
+    const savedRecipe = await newRecipe.save();
     res.status(201).json(savedRecipe);
-  });
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to save the recipe.' });
+  }
 });
 
 // Default route for the root path ("/")
