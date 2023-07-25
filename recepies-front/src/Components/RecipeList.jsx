0a5a585e542c1dc2fe65c33f84ae40b3f24b1df2
AndiZogaj22 <andiolsizogaj@gmail.com>
2023-07-25 23:06:01 +0200

    work on the cards

--- recepies-front/src/Components/RecipeList.jsx
+++ recepies-front/src/Components/RecipeList.jsx
@@ -10,27 +10,35 @@ const App = () => {
       setRecipes(response.data);
     });
   }, []);
+// Function to truncate the description text to a maximum of 100 characters
+const truncateDescription = (description) => {
+  const maxLength = 100;
+  return description.length > maxLength ? description.slice(0, maxLength) + '...' : description;
+};
 
-  return (
-    <div>
-      <div className="recipe-container" style={{ display: "flex", flexWrap: "wrap", gap: "20px" }}>
-        {recipes.map((recipe, index) => (
-          <div key={recipe._id} className="card w-96 bg-base-100 shadow-xl" style={{ margin: "10px" }}>
-            <figure className="px-10 pt-10">
-              <img src={recipe.image} alt={recipe.name} className="rounded-xl" />
-            </figure>
-            <div className="card-body items-center text-center">
-              <h2 className="card-title">{recipe.name}</h2>
-              <p>{recipe.description}</p>
-              <div className="card-actions">
-                <button className="btn btn-primary">Get Recipe</button>
-              </div>
-            </div>
+return (
+  <div>
+  <div className="recipe-container" style={{ display: "flex", flexWrap: "wrap", gap: "20px" }}>
+    {recipes.map((recipe, index) => (
+      <div key={recipe._id} className="card w-96 h-96 bg-base-100 shadow-xl pb-24" style={{ margin: "30px" }}>
+        <figure className="px-10 pt-15" style={{ height: "50%" }}>
+          <img src={recipe.image} alt={recipe.name} className="rounded-xl" style={{ width: "100%", height: "100%", objectFit: "cover" }} />
+        </figure>
+        <div className="card-body items-center text-center" style={{ height: "50%" }}>
+          <h2 className="card-title">{recipe.name}</h2>
+          <h2 className="badge badge-outline p-3">{recipe.country}</h2>
+          <p>{truncateDescription(recipe.description)}</p>
+          <div className="card-actions ">
+            <button className="btn btn-outline btn-info">Get Recipe</button>
           </div>
-        ))}
+        </div>
       </div>
-    </div>
-  );
+    ))}
+  </div>
+</div>
+
+
+);
 };
 
 export default App;
