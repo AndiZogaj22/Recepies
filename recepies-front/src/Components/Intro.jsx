3cc419d0b2f36a6596e6d4d9f8b40f275a492ce6
AndiZogaj22 <andiolsizogaj@gmail.com>
2023-07-25 23:24:16 +0200

    working on the banner

--- recepies-front/src/Components/Intro.jsx
+++ recepies-front/src/Components/Intro.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 const Intro = () => {
   return (
-    <div>
-    <h1>Welcome to TopRecepies</h1>
-    <p>Search for the recepie you are looking or choose one of the categoeies below.</p>
-   </div>
+    <div className="bg-blue-500 py-8 mt-8">
+    <div className="container mx-auto text-center">
+      <h1 className="text-white text-4xl font-bold mb-4">Welcome to MediterraneanCuisine</h1>
+      <p className="text-white text-lg">"Unlock the secrets of Mediterranean cuisine, where every bite is a journey to the heart of flavor and tradition."</p>
+    </div>
+  </div>
   )
 }
 
