346ac98fd8c970f3b47730028f7b6b42eb5c4701
AndiZogaj22 <andiolsizogaj@gmail.com>
2023-07-26 00:05:39 +0200

    workin on navbar router problem and link not working

--- recepies-front/src/App.jsx
+++ recepies-front/src/App.jsx
@@ -5,12 +5,15 @@ import Categories from './Components/Categories';
 import Intro from './Components/Intro';
 import RecipeList from './Components/RecipeList';
 import Navbar from './Components/Navbar';
+import { Route,Routes } from 'react-router-dom';
+import TotalRecipes from './pages/TotalRecipes';
 function App() {
   return (
     <>
       <Navbar />
       <div className="flex justify-center items-center">
         <Intro />
+        
       </div>
       <div className="flex justify-center items-center pt-10">
         <SearchBar />
@@ -20,7 +23,6 @@ function App() {
       </div>
       <div className="container mx-auto py-8">
         <RecipeList />
-        
       </div>
     </>
   );
--- recepies-front/src/Components/Navbar.jsx
+++ recepies-front/src/Components/Navbar.jsx
@@ -1,33 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiHome, FiBook, FiHeart } from 'react-icons/fi';
+import { GiCook } from 'react-icons/gi';
 
 const Navbar = () => {
   return (
     <nav className="bg-white p-4 flex justify-between items-center shadow-md">
-      {/* Logo */}
       <div className="flex items-center">
-        <FiBook className="text-primary text-3xl mr-2" />
+        <GiCook className="text-primary text-3xl mr-2" />
         <span className="text-primary font-bold text-xl">MediterraneanCuisine</span>
       </div>
 
-      {/* Navigation links */}
       <ul className="flex space-x-4">
         <li>
-          <Link to="/" className="text-primary hover:text-secondary">
-            <FiHome className="text-xl" />
-          </Link>
+          <div className="link">
+            <h3 className="text-xl hover:underline hover:text-orange-500">Home</h3>
+          </div>
         </li>
         <li>
-          <Link to="/recipes" className="text-primary hover:text-secondary">
-            <FiBook className="text-xl" />
-          </Link>
+          <div className="link">
+            <h3 className="text-xl hover:underline hover:text-orange-500">Recipes</h3>
+          </div>
         </li>
       </ul>
 
-      {/* Saved Recipes button */}
-      <button className="btn btn-primary">
-        <FiHeart className="text-xl" />
+      <button className="btn btn-outline btn-success">
+        <FiHeart className="text-xl" />My Recipes
       </button>
     </nav>
   );
