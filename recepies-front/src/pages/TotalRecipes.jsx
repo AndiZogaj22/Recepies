c146a150a0a83c5523a457f49fc086ef57343395
AndiZogaj22 <andiolsizogaj@gmail.com>
2023-07-26 23:53:12 +0200

    total or all recipes page

--- recepies-front/src/pages/TotalRecipes.jsx
+++ recepies-front/src/pages/TotalRecipes.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Navbar from '../Components/Navbar'
+import '../App.css';
 
 const TotalRecipes = () => {
   return (
-    <div>TotalRecipes</div>
+    <div>TotalRecipes
+        <Navbar />
+    </div>
   )
 }
 
