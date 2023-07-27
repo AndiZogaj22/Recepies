379bd3fc7e68ecb7f3229f8d4fe0fb5b3040298b
AndiZogaj22 <andiolsizogaj@gmail.com>
2023-07-27 21:39:43 +0200

    navbar done meaby needs so effects

--- recepies-front/src/Components/Navbar.jsx
+++ recepies-front/src/Components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { GiCook } from 'react-icons/gi';
+import { FiHome, FiBookOpen } from 'react-icons/fi';
+import { FiHeart } from 'react-icons/fi';
 
 const navigations = [
   {
@@ -19,23 +21,37 @@ const navigations = [
 
 const Navbar = () => {
   return (
-    <nav className="bg-white p-4 flex justify-between items-center shadow-md">
+    <nav className="bg-white p-4 mt-5 flex justify-between items-center shadow-md">
       <div className="flex items-center">
-        <GiCook className="text-primary text-3xl mr-2" />
-        <span className="text-primary font-bold text-xl">MediterraneanCuisine</span>
+        <GiCook className="text-amber-600	 text-3xl mr-2" />
+        <span className="text-amber-600	 font-bold text-xl">MediterraneanCuisine</span> {/* Updated the text color to green */}
+
       </div>
 
-      <ul className="flex space-x-4">
+      <ul className="flex space-x-4 items-center ml-auto list-none"> {/* Added list-none class */}
         {navigations.map((navigation) => (
-          <li key={navigation.path}>
-            <Link to={navigation.path} className="link">
-              <h3 className="text-xl hover:underline hover:text-orange-500">{navigation.name}</h3>
-            </Link>
+          <li key={navigation.path} className="list-none 	text-decoration-line: none;"> {/* Added list-none class */}
+            {navigation.name === 'My Recipes' ? (
+              <Link to={navigation.path}>
+                <button className="btn btn-outline btn-success flex items-center justify-center " style={{ minWidth: '120px' }}>
+                  <FiHeart className="text-xl mr-1" />
+                  {navigation.name}
+                </button>
+              </Link>
+            ) : (
+              <Link
+                to={navigation.path}
+                className="link flex items-center justify-center text-decoration-none no-underline hover:text-amber-600	" // Updated hover effect and removed text decoration
+                style={{ minWidth: '120px' }}
+              >
+                {navigation.name === 'Home' && <FiHome className="text-lg mr-1" />}
+                {navigation.name === 'All Recipes' && <FiBookOpen className="text-lg mr-1" />}
+                {navigation.name}
+              </Link>
+            )}
           </li>
         ))}
       </ul>
-
-      
     </nav>
   );
 };
