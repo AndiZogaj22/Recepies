3181b6b271164aa591ca89454f34c9896cc303bc
AndiZogaj22 <andiolsizogaj@gmail.com>
2023-07-27 21:40:05 +0200

    imported to component to show the recipes

--- recepies-front/src/pages/TotalRecipes.jsx
+++ recepies-front/src/pages/TotalRecipes.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import '../App.css';
-
+import RecipeList from '../Components/RecipeList'
 const TotalRecipes = () => {
   return (
     <div>TotalRecipes
         <Navbar />
+        <RecipeList />
     </div>
   )
 }
