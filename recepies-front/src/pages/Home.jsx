36acd019fe7866fd16e2e9a9be3b1b6843f68e38
AndiZogaj22 <andiolsizogaj@gmail.com>
2023-07-27 22:24:35 +0200

    see all button

--- recepies-front/src/pages/Home.jsx
+++ recepies-front/src/pages/Home.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import '../App.css';
-import Intro from '../Components/Intro'
+import '../ScrollBar.css';
+import ImageCarousel from '../Components/ImageCarousel'
 import SearchBar from '../Components/SearchBar'
 import Categories from '../Components/Categories'
 import RecipeList from '../Components/RecipeList'
 import Navbar from '../Components/Navbar';
+import { Link } from 'react-router-dom';
 const Home = () => {
   return (
    <>
      <Navbar />
       <div className="flex justify-center items-center">
-        <Intro />
+        <ImageCarousel /> 
       </div>
       <div className="flex justify-center items-center pt-10">
         <SearchBar />
@@ -20,6 +22,11 @@ const Home = () => {
       </div>
       <div className="container mx-auto py-8">
         <RecipeList />
+      </div>
+      <div className="text-center mt-4">
+        <Link to="/allrecipes">
+          <button className="btn btn-outline btn-warning">See All</button>
+        </Link>
       </div>
    </>
   )
