f8f4be0332f2953797827d540930f5a18705b14c
AndiZogaj22 <andiolsizogaj@gmail.com>
2023-07-29 13:00:32 +0200

    fixed the props error

--- recepies-front/src/pages/Home.jsx
+++ recepies-front/src/pages/Home.jsx
@@ -7,7 +7,7 @@ import RecipeList from '../Components/RecipeList';
 import Navbar from '../Components/Navbar';
 import { Link } from 'react-router-dom';
 
-const Home = () => {
+const Home = ({ handleSearch }) => {
   return (
     <>
       <Navbar />
@@ -15,12 +15,12 @@ const Home = () => {
         <ImageCarousel />
       </div>
       <div className="flex justify-center items-center pt-10">
-        <SearchBar />
+        <SearchBar handleSearch={handleSearch} />
       </div>
-      
+
       <div className="container mx-auto py-8">
         {/* Limit the number of recipes to be shown to 9 on the Home page */}
-        <RecipeList limit={9} />
+        <RecipeList limit={9} searchQuery="" />
       </div>
       <div className="text-center mt-4 pb-10">
         <Link to="/allrecipes">
