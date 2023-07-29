b6c2a59a05e9d30de5cdfdde469199337cc7fd8c
AndiZogaj22 <andiolsizogaj@gmail.com>
2023-07-29 12:20:55 +0200

    .

--- recepies-front/src/pages/Home.jsx
+++ recepies-front/src/pages/Home.jsx
@@ -3,7 +3,6 @@ import '../App.css';
 import '../ScrollBar.css';
 import ImageCarousel from '../Components/ImageCarousel';
 import SearchBar from '../Components/SearchBar';
-import Categories from '../Components/Categories';
 import RecipeList from '../Components/RecipeList';
 import Navbar from '../Components/Navbar';
 import { Link } from 'react-router-dom';
@@ -18,14 +17,12 @@ const Home = () => {
       <div className="flex justify-center items-center pt-10">
         <SearchBar />
       </div>
-      <div className="container mx-auto py-8">
-        <Categories />
-      </div>
+      
       <div className="container mx-auto py-8">
         {/* Limit the number of recipes to be shown to 9 on the Home page */}
         <RecipeList limit={9} />
       </div>
-      <div className="text-center mt-4">
+      <div className="text-center mt-4 pb-10">
         <Link to="/allrecipes">
           <button className="btn btn-outline btn-warning">See All</button>
         </Link>
