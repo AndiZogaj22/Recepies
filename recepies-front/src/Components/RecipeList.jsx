0cfa5fa1ab9a15f2716bf21765edd4b3852d49c5
AndiZogaj22 <andiolsizogaj@gmail.com>
2023-07-29 13:01:03 +0200

    cards will be filtred in real time by the input

--- recepies-front/src/Components/RecipeList.jsx
+++ recepies-front/src/Components/RecipeList.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
-const RecipeList = ({ limit }) => {
+const RecipeList = ({ limit, searchQuery }) => {
   const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
@@ -18,18 +18,25 @@ const RecipeList = ({ limit }) => {
     return description.length > maxLength ? description.slice(0, maxLength) + '...' : description;
   };
 
-  // Limit the number of recipes to be shown based on the 'limit' prop
-  const limitedRecipes = recipes.slice(0, limit);
+  // Filter recipes based on the search query
+  const filteredRecipes = recipes
+    .filter((recipe) => recipe.name.toLowerCase().includes(searchQuery.toLowerCase()))
+    .slice(0, limit);
 
   return (
     <div>
-      <div className="recipe-container" style={{ display: "flex", flexWrap: "wrap", gap: "20px" }}>
-        {limitedRecipes.map((recipe, index) => (
-          <div key={recipe._id} className="card w-96 h-96 bg-base-100 shadow-xl pb-24" style={{ margin: "30px" }}>
-            <figure className="px-10 pt-15" style={{ height: "50%" }}>
-              <img src={recipe.image} alt={recipe.name} className="rounded-xl" style={{ width: "100%", height: "100%", objectFit: "cover" }} />
+      <div className="recipe-container" style={{ display: 'flex', flexWrap: 'wrap', gap: '20px' }}>
+        {filteredRecipes.map((recipe) => (
+          <div key={recipe._id} className="card w-96 h-96 bg-base-100 shadow-xl pb-24" style={{ margin: '30px' }}>
+            <figure className="px-10 pt-15" style={{ height: '50%' }}>
+              <img
+                src={recipe.image}
+                alt={recipe.name}
+                className="rounded-xl"
+                style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+              />
             </figure>
-            <div className="card-body items-center text-center" style={{ height: "50%" }}>
+            <div className="card-body items-center text-center" style={{ height: '50%' }}>
               <h2 className="card-title">{recipe.name}</h2>
               <h2 className="badge badge-outline p-3">{recipe.country}</h2>
               <p>{truncateDescription(recipe.description)}</p>
