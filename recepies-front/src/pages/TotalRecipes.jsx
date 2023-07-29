6596864285079a86ca69bf86258ac1b65970df4c
AndiZogaj22 <andiolsizogaj@gmail.com>
2023-07-29 13:01:18 +0200

    props call

--- recepies-front/src/pages/TotalRecipes.jsx
+++ recepies-front/src/pages/TotalRecipes.jsx
@@ -1,23 +1,30 @@
-import React from 'react'
-import Navbar from '../Components/Navbar'
+import React, { useState } from 'react';
+import Navbar from '../Components/Navbar';
 import '../App.css';
-import RecipeList from '../Components/RecipeList'
+import RecipeList from '../Components/RecipeList';
 import '../ScrollBar.css';
-import Categories from '../Components/Categories'
+import Categories from '../Components/Categories';
 import SearchBar from '../Components/SearchBar';
+
 const TotalRecipes = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+  };
+
   return (
     <div>
-        <Navbar />
-        <div className="container mx-auto py-8">
+      <Navbar />
+      <div className="container mx-auto py-8">
         <Categories />
       </div>
       <div className="flex justify-center items-center pt-10">
-        <SearchBar />
+        <SearchBar handleSearch={handleSearch} />
       </div>
-        <RecipeList />
+      <RecipeList limit={10} searchQuery={searchQuery} />
     </div>
-  )
-}
+  );
+};
 
-export default TotalRecipes
+export default TotalRecipes;
