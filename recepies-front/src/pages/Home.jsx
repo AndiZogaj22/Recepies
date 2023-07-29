3f453838e3c6a54f911a588797c7d4100bfa852e
AndiZogaj22 <andiolsizogaj@gmail.com>
2023-07-30 01:18:31 +0200

    button for refreshing and giving new recipes each times clicked

--- recepies-front/src/pages/Home.jsx
+++ recepies-front/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
 import '../ScrollBar.css';
 import ImageCarousel from '../Components/ImageCarousel';
@@ -8,6 +8,13 @@ import Navbar from '../Components/Navbar';
 import { Link } from 'react-router-dom';
 
 const Home = ({ handleSearch }) => {
+  const [refreshKey, setRefreshKey] = useState(0);
+
+  const handleRefreshClick = () => {
+    // Update the refreshKey to trigger a re-render of the HomeList component with new recipes
+    setRefreshKey((prevKey) => prevKey + 1);
+  };
+
   return (
     <>
       <Navbar />
@@ -16,12 +23,17 @@ const Home = ({ handleSearch }) => {
       </div>
       <div className="flex justify-center items-center pt-10">
         <h1 className="max-w-lg text-3xl font-semibold leading-loose text-gray-900 dark:text-white">
-          Top Recipes
+           Recommend Recipes 
+         
+          <div className="flex justify-center items-center pt-10">
+          <button className="btn btn-outline btn-secondary" onClick={handleRefreshClick}>Refresh</button>
+          </div>
         </h1>
+
       </div>
       <div className="container mx-auto py-8">
         {/* Limit the number of recipes to be shown to 9 on the Home page */}
-        <HomeList limit={9} searchQuery="" />
+        <HomeList key={refreshKey} limit={9} searchQuery="" />
       </div>
       <div className="text-center mt-4 pb-10">
         <Link to="/allrecipes">
