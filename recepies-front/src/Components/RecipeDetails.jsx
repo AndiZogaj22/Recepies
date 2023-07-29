f6bd7dba00a79bace3026102c9cc8474fd144200
AndiZogaj22 <andiolsizogaj@gmail.com>
2023-07-29 12:23:02 +0200

    now the disaply button works

--- recepies-front/src/Components/RecipeDetails.jsx
+++ recepies-front/src/Components/RecipeDetails.jsx
@@ -1,24 +1,34 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
-
+import Navbar from '../Components/Navbar'
 const RecipeDetails = () => {
   const location = useLocation();
-  const { recipe } = location.state;
+
+  // Check if 'recipe' exists in location.state, otherwise set it to null
+  const recipe = location.state?.recipe || null;
+
+  // If recipe is null or undefined, display an error message or redirect to a fallback page
+  if (!recipe) {
+    return <div className="text-center mt-8">Error: Recipe not found</div>; // You can return a more informative error message or use Redirect to redirect to a fallback page
+  }
 
   return (
-    <div>
-      <h2>{recipe.name}</h2>
-      <p>{recipe.description}</p>
-      <p>Country: {recipe.country}</p>
-      <h3>Ingredients:</h3>
-      <ul>
+    <>
+    <Navbar />
+    <div className="max-w-xl mx-auto p-4 mt-8">
+      <h2 className="text-2xl font-bold mb-4">{recipe.name}</h2>
+      <p className="text-gray-600 mb-4">{recipe.description}</p>
+      <p className="text-gray-600">Country: {recipe.country}</p>
+      <h3 className="text-xl font-semibold mt-6">Ingredients:</h3>
+      <ul className="list-disc list-inside mt-2">
         {recipe.ingredients.map((ingredient, index) => (
-          <li key={index}>{ingredient}</li>
+          <li key={index} className="text-gray-600">{ingredient}</li>
         ))}
       </ul>
-      <img src={recipe.image} alt={recipe.name} />
-      <p>Tutorial: {recipe.tutorial}</p>
+      <img src={recipe.image} alt={recipe.name} className="mt-6 rounded-lg shadow-xl" style={{ width: '100%', height: 'auto' }} />
+      <p className="text-gray-600 mt-4">Tutorial: {recipe.tutorial}</p>
     </div>
+    </>
   );
 };
 
--- recepies-front/src/pages/TotalRecipes.jsx
+++ recepies-front/src/pages/TotalRecipes.jsx
@@ -3,11 +3,18 @@ import Navbar from '../Components/Navbar'
 import '../App.css';
 import RecipeList from '../Components/RecipeList'
 import '../ScrollBar.css';
-
+import Categories from '../Components/Categories'
+import SearchBar from '../Components/SearchBar';
 const TotalRecipes = () => {
   return (
-    <div>TotalRecipes
+    <div>
         <Navbar />
+        <div className="container mx-auto py-8">
+        <Categories />
+      </div>
+      <div className="flex justify-center items-center pt-10">
+        <SearchBar />
+      </div>
         <RecipeList />
     </div>
   )
